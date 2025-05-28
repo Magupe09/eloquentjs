5938d90715644ba5e865e637c647bae7ae285167
Mauricio <mgp.94@hotmail.com>
2025-05-28 16:07:32 -0500

    Manejo de errorores

---
+++ toString.js
@@ -0,0 +1,37 @@
+function numeroAString(n, base = 10) {
+    let resultado = "", signo = "";
+    if (n < 0) {
+      signo = "-";
+      n = -n;
+    }
+    do {
+      resultado = String(n % base) + resultado;
+      n /= base;
+    } while (n > 0);
+    return signo + resultado;
+  }
+  console.log(numeroAString(13, 10));
+  // → 1.5e-3231.3e-3221.3e-3211.3e-3201.3e-3191.3e-3181.3…
+
+class FalloUnidadMultiplicadora extends Error {}
+
+function multiplicacionPrimitiva(a, b) {
+  if (Math.random() < 0.2) {
+    let res=a * b;
+    //console.log(res)
+    return res
+  } else {
+    throw new FalloUnidadMultiplicadora("Klunk");
+  }
+}
+
+function multiplicacionConfiable(a, b) {
+  // Tu código aqui.
+  if(multiplicacionPrimitiva(a,b)){
+     return 
+  }
+}
+
+//console.log(multiplicacionPrimitiva(8, 8));
+console.log(multiplicacionConfiable(8, 8));
+// → 64
