c8920107d19d66c62102ee05085784310dfaec14
Mauricio <mgp.94@hotmail.com>
2025-05-29 16:42:44 -0500

    Function primitiva

--- toString.js
+++ toString.js
@@ -10,16 +10,14 @@ function numeroAString(n, base = 10) {
     } while (n > 0);
     return signo + resultado;
   }
-  console.log(numeroAString(13, 10));
+  //console.log(numeroAString(13, 10));
   // → 1.5e-3231.3e-3221.3e-3211.3e-3201.3e-3191.3e-3181.3…
 
 class FalloUnidadMultiplicadora extends Error {}
 
 function multiplicacionPrimitiva(a, b) {
   if (Math.random() < 0.2) {
-    let res=a * b;
-    //console.log(res)
-    return res
+    return a*b
   } else {
     throw new FalloUnidadMultiplicadora("Klunk");
   }
@@ -27,11 +25,21 @@ function multiplicacionPrimitiva(a, b) {
 
 function multiplicacionConfiable(a, b) {
   // Tu código aqui.
-  if(multiplicacionPrimitiva(a,b)){
-     return 
+  while(true){
+    try {
+      resultado=multiplicacionPrimitiva(a, b)
+      return resultado
+    } catch (e) {
+      if (e instanceof FalloUnidadMultiplicadora){
+        
+        return respuesta= multiplicacionPrimitiva(a,b);
+      }else{
+        throw e;
+      }
   }
+}
 }
 
 //console.log(multiplicacionPrimitiva(8, 8));
-console.log(multiplicacionConfiable(8, 8));
+console.log(multiplicacionConfiable(9, 8));
 // → 64
